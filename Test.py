fdfd0fbd332c3e46ca4b42276358b38065ed513c
Rafif1802 <165453968+Rafif1802@users.noreply.github.com>
2024-03-30 11:56:17 +0700

    inil

---
+++ Test.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.header('Aplikasi Penjumlahan Bilangan Numerik', divider = 'rainbow')
+
+angka_pertama = st.number_input('Masukkan Angka pertama')
+st.write('Angka pertama adalah',angka_pertama)
+
+angka_kedua = st.number_input('Masukkan Angka kedua')
+st.write('Angka kedua adalah',angka_kedua)
+
+operasi_matematika_pertambahan = angka_pertama+angka_kedua
+operasi_matematika_pengurangan = angka_pertama-angka_kedua
+operasi_matematika_perkalian = angka_pertama*angka_kedua
+operasi_matematika_pembagian = angka_pertama/angka_kedua
+
+st.write(f"Angka pertama {angka_pertama} + Angka kedua {angka_kedua} = {operasi_matematika_pertambahan}")
+st.write(f"Angka pertama {angka_pertama} - Angka kedua {angka_kedua} = {operasi_matematika_pengurangan}")
+st.write(f"Angka pertama {angka_pertama} / Angka kedua {angka_kedua} = {operasi_matematika_pembagian}")
+st.write(f"Angka pertama {angka_pertama} x Angka kedua {angka_kedua} = {operasi_matematika_perkalian}")
