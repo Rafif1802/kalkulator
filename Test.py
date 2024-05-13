c58133abafa58845474389588c5880c257748e2c
Rafif1802 <165453968+Rafif1802@users.noreply.github.com>
2024-05-13 13:30:53 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -17,3 +17,5 @@ st.write(f"Angka pertama {angka_pertama} + Angka kedua {angka_kedua} = {operasi_
 st.write(f"Angka pertama {angka_pertama} - Angka kedua {angka_kedua} = {operasi_matematika_pengurangan}")
 st.write(f"Angka pertama {angka_pertama} / Angka kedua {angka_kedua} = {operasi_matematika_pembagian}")
 st.write(f"Angka pertama {angka_pertama} x Angka kedua {angka_kedua} = {operasi_matematika_perkalian}")
+
+st.image("https://res.cloudinary.com/dk0z4ums3/image/upload/v1627618536/attached_image/mengingatkan-kembali-kepada-manfaat-karbohidrat.jpg", width=300)
